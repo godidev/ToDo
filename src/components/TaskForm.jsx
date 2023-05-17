4c65ac7d84b7fe9b187c6187972afa12419c41b2
Gorka Diez <99983319+godidev@users.noreply.github.com>
2023-05-17 19:07:23 +0200

    feat: prevent adding empty tasks

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -2,6 +2,7 @@ import { useState, useId } from 'react'
 
 export default function TaskForm({ onSubmit }) {
   const [newTask, setNewTask] = useState('')
+  const [alert, setAlert] = useState()
   const id = useId()
 
   const handleChange = e => {
@@ -10,8 +11,13 @@ export default function TaskForm({ onSubmit }) {
 
   const handleSubmit = e => {
     e.preventDefault()
-    onSubmit({ task: newTask, done: false, id: id + newTask })
-    setNewTask('')
+    if (newTask) {
+      onSubmit({ task: newTask, done: false, id: id + newTask })
+      setNewTask('')
+    } else {
+      setAlert("You can't add an empty task")
+      setTimeout(() => setAlert(null), 5000)
+    }
   }
 
   return (
@@ -24,6 +30,7 @@ export default function TaskForm({ onSubmit }) {
         onChange={handleChange}
         placeholder='Do laundry, clean bedroom...'
       />
+      {alert && <p className='alert-msg'>{alert}</p>}
       <button>Add task</button>
     </form>
   )
